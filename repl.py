c813ccd6b8c2fe39b8836f51732e447d28849a5d
user <mixal4enko@gmail.com>
2014-12-05 14:40:45 +0400

    separate repl

---
+++ repl.py
@@ -0,0 +1,9 @@
+from lexer import makelist
+from evaluator import calculate
+
+if __name__ == "__main__":
+    while True:
+        line = raw_input().rstrip()
+        if not line: 
+            break
+        print calculate(makelist(line))
