294b01e5da500f3d1b93229daf0ba91f8fb64cb2
user <mixal4enko@gmail.com>
2014-12-05 11:14:02 +0400

    simple lexer and repl

---
+++ lexer.py
@@ -0,0 +1,16 @@
+""" perform lexical analysys """
+import re
+
+def makelist(string):
+    string = re.sub(r'\(',r'[', string, 400)
+    string = re.sub(r'\)',r']', string, 400)
+    string = re.sub(r'([\w.]+)',r'"\1"', string, 400)
+    string = re.sub(r'(\s+)',r', ', string, 400)
+    return string
+
+if __name__ == "__main__":
+    from evaluator import calculate
+    while True:
+        line = raw_input().rstrip()
+        if not line: break
+        print calculate(eval(makelist(line)))
