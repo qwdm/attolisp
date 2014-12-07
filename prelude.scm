39e8abd9a51c8d9b486627a16a9034f3636abd00
user <mixal4enko@gmail.com>
2014-12-07 13:49:30 +0400

    add common list funtions

--- prelude.scm
+++ prelude.scm
@@ -11,7 +11,6 @@
     (cons (f (car l))
           (map f (cdr l)))))
 
-
 (define (filter f lst)
   (if (empty? lst)
     None
@@ -19,3 +18,30 @@
       (cons (car lst)
             (filter f (cdr lst)))
       (filter f (cdr lst)))))
+
+(define (length lst)
+  (if (empty? lst)
+    0
+    (+ 1
+       (length (cdr lst)))))
+
+(define (reverse lst)
+  (if (= 1 (length lst))
+    (list (car lst))
+    (append (reverse (cdr lst))
+            (list (car lst)))))
+
+; reduce in python style with init (leftmost) value
+(define (reduce f init lst)
+  (if (empty? lst)
+    init
+    (reduce f
+            (f init (car lst))
+            (cdr lst))))
+
+; (range a b) -> list from a to b inclusively
+(define (range a b)
+  (if (= a b)
+    (list a)
+    (cons a
+          (range (inc a) b))))
